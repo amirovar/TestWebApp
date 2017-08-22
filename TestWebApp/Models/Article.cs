3d32677a18a48d77e18b9023427177bfa7f35f6f
Regina Gimadieva <regina.gimadieva@outlook.com>
2017-08-22 21:58:13 +0300

    Getting started: Add controller, view and model

---
+++ TestWebApp/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TestWebApp.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set;}
+        public DateTime ReleaseDateP { get; set; }
+        public string ImgPath { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class ArticleDBContext : DbContext
+    {
+        public DbSet<Article> Articlies { get; set; }
+    }
+}
